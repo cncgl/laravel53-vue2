93ff1e14df3b4f74df3dfcd2a689ed0b1b848dbf
cncgl <concigel@gmail.com>
2017-03-17 00:48:23 +0900

    fix for phpcs

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,7 +25,7 @@ class PostController extends Controller
   /**
    * Store a newly created resource in storage.
    *
-   * @param  \Illuminate\Http\Request  $request
+   * @param \Illuminate\Http\Request $request
    * @return \Illuminate\Http\Response
    */
   public function store(Request $request)
@@ -40,7 +40,7 @@ class PostController extends Controller
   /**
    * Display the specified resource.
    *
-   * @param  int  $id
+   * @param int $id
    * @return \Illuminate\Http\Response
    */
   public function show($id)
@@ -52,8 +52,8 @@ class PostController extends Controller
   /**
    * Update the specified resource in storage.
    *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
+   * @param \Illuminate\Http\Request $request
+   * @param int                      $id
    * @return \Illuminate\Http\Response
    */
   public function update(Request $request, $id)
@@ -68,7 +68,7 @@ class PostController extends Controller
   /**
    * Remove the specified resource from storage.
    *
-   * @param  int  $id
+   * @param int $id
    * @return \Illuminate\Http\Response
    */
   public function destroy($id)
